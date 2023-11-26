93c5ab0dc1ba16ec00394519c37c9a5c76989f8a
skyepodium <skyepodium@gmail.com>
2023-11-26 20:06:20 +0900

    [코드 수정] createdAt, modifiedAt의 타입을 String으로 변경
    [코드 수정] createdAt, modifiedAt의 타입을 String으로 변경

--- web/union-sql-inejection-mybatis/blog/src/main/java/com/example/blog/domain/Post.java
+++ web/union-sql-inejection-mybatis/blog/src/main/java/com/example/blog/domain/Post.java
@@ -3,14 +3,12 @@ package com.example.blog.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.sql.Timestamp;
-
 @Getter
 @Setter
 public class Post {
     private long postSeq;
     private String title;
     private String content;
-    private Timestamp createdAt;
-    private Timestamp modifiedAt;
+    private String createdAt;
+    private String modifiedAt;
 }
